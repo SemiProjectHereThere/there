4481215c9da32c59c128f9682eaab39a8ed50bdd
서동진 <user1@KH1>
2016-09-22 12:41:00 +0900

    글 삽입 커밋함

--- HereThereWebProject/src/board/controller/BoardInsertServlet.java
+++ HereThereWebProject/src/board/controller/BoardInsertServlet.java
@@ -3,11 +3,13 @@ package board.controller;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.websocket.SendResult;
 
 import board.model.service.BoardService;
 import board.model.vo.Board;
@@ -40,27 +42,17 @@ public class BoardInsertServlet extends HttpServlet {
 		String location = request.getParameter("location");
 		String category = request.getParameter("category");
 		String content = request.getParameter("content");
-		String mapinfo = request.getParameter("xy");
+		String map = request.getParameter("xy");
 		
-		ArrayList<String> x = new ArrayList<String>();
-		ArrayList<String> y = new ArrayList<String>();
-		String mapArr[] = mapinfo.split("/");
-		for(int i=0; i<mapArr.length; i++){
-			x.add(mapArr[i].substring(mapArr[i].indexOf("(")+1, mapArr[i].indexOf(",")));
-			y.add(mapArr[i].substring(mapArr[i].indexOf(",")+1, mapArr[i].indexOf(")")));
-		}
-		
-		
-		
-		System.out.println(title + "," + id + "," + location + "," + category + "," + content + "," + mapinfo);
-		
-		Board board = new Board();
+		Board board = new Board(title, content, id, category, location, map);
 		int result = new BoardService().insertBoard(board);
 		
 		if(result > 0){
-			//성공시 게시판 리스트로 sendRedirect
+			response.sendRedirect("index.jsp");
 		}else{
-			//실패시 RequestDispatcher
+			RequestDispatcher error = request.getRequestDispatcher("board/boardError.jsp");
+			request.setAttribute("code", "BoardInsert");
+			error.forward(request, response);
 		}
 	}
 
