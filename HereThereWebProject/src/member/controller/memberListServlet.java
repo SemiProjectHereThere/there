febfa4876753014252730e36663519e3fe1315e8
Administrator <derst_bulture@hanmail.net>
2016-09-29 20:05:03 +0900

    멤머리스트서블릿 추가

---
+++ HereThereWebProject/src/member/controller/memberListServlet.java
@@ -0,0 +1,66 @@
+package member.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import member.model.service.MemberService;
+import member.model.vo.Member;
+
+/**
+ * Servlet implementation class memberListServlet
+ */
+@WebServlet("/mall")
+public class memberListServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public memberListServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+	      response.setContentType("text/html; charset=utf-8");
+	      
+	      ArrayList<Member> list = new MemberService().selectAll();
+	      String userId = request.getParameter("userid");
+	      String userName = request.getParameter("username");
+	      String managerYN = request.getParameter("manageryn");
+	      int pg = Integer.parseInt(request.getParameter("pg"));
+	      
+	      RequestDispatcher view = null;
+	      if(list != null){
+	         view = request.getRequestDispatcher("admin/adminMemberListView.jsp");
+	         request.setAttribute("list", list);
+	         request.setAttribute("userid", userId);
+	         request.setAttribute("username", userName);
+	         request.setAttribute("manageryn", managerYN);
+	         request.setAttribute("pg", pg);
+	         view.forward(request, response);
+	      }else{
+	         
+	      }
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
